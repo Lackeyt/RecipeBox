efae42a1893f268427119a6aa691fe5bd7805c1f
Tyson Lackey <lackeyt90@gmail.com>
2020-08-10 12:14:31 -0700

    update file baseline logic
    co-authored-by: Frederick Ernest <frederick.ernest@gmail.com>

---
+++ RecipeBox/Models/ApplicationUser.cs
@@ -0,0 +1,9 @@
+using Microsoft.AspNetCore.Identity;
+
+namespace Recipebox.Models
+{
+    public class ApplicationUser : IdentityUser
+    {
+
+    }
+}
---
+++ RecipeBox/Models/RecipeBoxContext.cs
@@ -0,0 +1,10 @@
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace RecipeBox.Models
+{
+  public class RecipeBoxContext : IdentityDbContext<ApplicationUser>
+  {
+    
+  }
+}
