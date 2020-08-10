9c3cc845c886ebf2e9eb65b02696ef7f77c9ff97
Tyson Lackey <lackeyt90@gmail.com>
2020-08-10 15:47:21 -0700

    update controller logic
    co-authored-by: Frederick Ernest <frederick.ernest@gmail.com>

---
+++ RecipeBox/Controllers/RecipesController.cs
@@ -0,0 +1,130 @@
+using Microsoft.AspNetCore.Mvc;
+using RecipeBox.Models;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Mvc.Rendering;
+
+//new using directives
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using System.Threading.Tasks;
+using System.Security.Claims;
+
+namespace RecipeBox.Controllers
+{
+  [Authorize] //new line
+  public class RecipesController : Controller
+  {
+    private readonly RecipeBoxContext _db;
+    private readonly UserManager<ApplicationUser> _userManager; //new line
+
+    //updated constructor
+    public RecipesController(UserManager<ApplicationUser> userManager, RecipeBoxContext db)
+    {
+      _userManager = userManager;
+      _db = db;
+    }
+
+    //updated Index method
+    public async Task<ActionResult> Index()
+    {
+      var userId = this.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+      var currentUser = await _userManager.FindByIdAsync(userId);
+      var userItems = _db.Items.Where(entry => entry.User.Id == currentUser.Id);
+      return View(userItems);
+    }
+
+    public ActionResult Create()
+    {
+      ViewBag.UserId = new SelectList(_db.Users, "UserId", "Name");
+      return View();
+    }
+
+    //updated Create post method
+    [HttpPost]
+    public async Task<ActionResult> Create(Recipe recipe, int UserId)
+    {
+      var userId = this.User.FindFirst(ClaimTypes.NameIdentifier)?.Value;
+      var currentUser = await _userManager.FindByIdAsync(userId);
+      recipe.User = currentUser;
+      _db.Recipes.Add(recipe);
+      if (UserId != 0)
+      {
+        _db.UserRecipe.Add(new UserRecipe() { UserId = UserId, RecipeId = recipe.RecipeId });
+      }
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    public ActionResult Details(int id)
+    {
+      var thisRecipe = _db.Recipes
+          .Include(recipe => recipe.Users)
+          .ThenInclude(join => join.User)
+          .FirstOrDefault(recipe => recipe.RecipeId == id);
+      return View(thisRecipe);
+    }
+
+    public ActionResult Edit(int id)
+    {
+      var thisRecipe = _db.Recipes.FirstOrDefault(recipe => recipe.RecipeId == id);
+      ViewBag.UserId = new SelectList(_db.Users, "UserId", "Name");
+      return View(thisRecipe);
+    }
+
+    [HttpPost]
+    public ActionResult Edit(Recipe recipe, int UserId)
+    {
+      if (UserId != 0)
+      {
+        _db.UserRecipe.Add(new UserRecipe() { UserId = UserId, RecipeId = recipe.RecipeId });
+      }
+      _db.Entry(recipe).State = EntityState.Modified;
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    public ActionResult AddUser(int id)
+    {
+      var thisRecipe = _db.Recipes.FirstOrDefault(Recipes => recipe.RecipeId == id);
+      ViewBag.UserId = new SelectList(_db.Users, "UserId", "Name");
+      return View(thisRecipe);
+    }
+
+    [HttpPost]
+    public ActionResult AddUser(Recipe recipe, int UserId)
+    {
+      if (UserId != 0)
+      {
+        _db.UserRecipe.Add(new UserRecipe() { UserId = UserId, RecipeId = recipe.RecipeId });
+      }
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    public ActionResult Delete(int id)
+    {
+      var thisRecipe = _db.Recipes.FirstOrDefault(recipe => recipse.RecipeId == id);
+      return View(thisRecipe);
+    }
+
+    [HttpPost, ActionName("Delete")]
+    public ActionResult DeleteConfirmed(int id)
+    {
+      var thisRecipe = _db.Recipes.FirstOrDefault(recipe => recipes.recipeId == id);
+      _db.Recipe.Remove(thisRecipe);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+
+    [HttpPost]
+    public ActionResult DeleteUser(int joinId)
+    {
+      var joinEntry = _db.UserRecipe.FirstOrDefault(entry => entry.UserRecipeId == joinId);
+      _db.UserRecipe.Remove(joinEntry);
+      _db.SaveChanges();
+      return RedirectToAction("Index");
+    }
+  }
+}
